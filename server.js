3b7b9c6d71df749dc923c941a21efdcf60abaeae
omunoz <steven.120189@gmail.com>
2021-05-14 12:27:10 -0400

    server.js

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
 const app = express();
-const session = require('express-session');
 const flash = require('connect-flash');
-const SECRET_SESSION = process.env.SECRET_SESSION;
+const session = require('express-session');
 const passport = require('./config/ppConfig');
 const isLoggedIn = require('./middleware/isLoggedIn');
+
+const SECRET_SESSION = process.env.SECRET_SESSION;
 
 app.set('view engine', 'ejs');
 
@@ -14,33 +15,17 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+
 app.use(session({
   secret: SECRET_SESSION,
   resave: false,
   saveUninitialized: true
 }));
 app.use(flash());
-app.use((req,res,next) => {
-  console.log(res.locals);
-  res.locals.alerts = req.flash();
-  res.locals.currentUser = req.user;
-  next();
-});
+
 
 app.use(passport.initialize());
-//add a session
 app.use(passport.session());
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.use('/auth', require('./controllers/auth'));
-
-app.get('/profile', isLoggedIn, (req, res) => {
-  const { id, name, email } = req.user.get(); 
-  res.render('profile', { id, name, email });
-});
 
 app.use((req, res, next) => {
   console.log('========= RES.LOCALS =================')
@@ -49,6 +34,18 @@ app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   next();
 });
+
+app.get('/', (req, res) => {
+  res.render('index', {});
+});
+
+app.get('/profile', isLoggedIn, (req, res) => {
+  const { id, name, email } = req.user.get();
+  res.render('profile', { id, name, email });
+});
+
+app.use('/auth', require('./controllers/auth'));
+
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
